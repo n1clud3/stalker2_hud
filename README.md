762ab709f9745b09de22b1629c5b92d275890c1b
Mykola Malovanyi <kolyabarabot07@gmail.com>
2022-12-30 22:53:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# S.T.A.L.K.E.R 2 HUD for Garry's Mod
+
+The project was written under 4 hours.
+
+## Authors
+
+- n1clud3 - general idea and basic realization;
+- PrikolMen - optimization and code enhancements.
+
+## License
+
+See [LICENSE](./LICENSE) file.
---
+++ lua/autorun/client/cl_stalker2_hud.lua
@@ -0,0 +1,98 @@
+-- by n1clud3 and PrikolMen:-b
+
+local addonName = 'Stalker2 HUD'
+
+local math_floor = math.floor
+local math_min = math.min
+local ScrW = ScrW
+local ScrH = ScrH
+local hook = hook
+
+local scrW, scrH = ScrW(), ScrH()
+local screenPercentage = math_min( scrW, scrH ) / 100
+hook.Add('OnScreenSizeChanged', addonName, function()
+    scrW, scrH = ScrW(), ScrH()
+    screenPercentage = math_min( scrW, scrH ) / 100
+end)
+
+local function ScreenPercentage( percentage )
+    if (percentage) then
+        return math_floor( screenPercentage * percentage )
+    end
+
+    return screenPercentage
+end
+
+-- La Config 👍
+-- In Scale Percents
+local hudWidth = 25
+local hudOffset = 5
+
+local armorHeight = 0.6
+local healthHeight = 1.2
+
+-- In Pixels
+local healthArmorOffset = 3
+
+-- Colors :)
+local alpha = 220
+local bg = Color( 20, 20, 20, alpha )
+local health = Color( 190, 44, 54, alpha )
+local armor = Color( 60, 155, 155, alpha )
+
+local surface_SetDrawColor = surface.SetDrawColor
+local surface_DrawRect = surface.DrawRect
+local FrameTime = FrameTime
+local Lerp = Lerp
+
+hook.Add('RenderScene', addonName, function()
+    hook.Remove( 'RenderScene', addonName )
+
+    local plyHealth, plyArmor = 0, 0
+    local ply = LocalPlayer()
+
+    hook.Add('HUDPaint', addonName, function()
+        local hw, hh = ScreenPercentage( hudWidth ), ScreenPercentage( healthHeight )
+        local offset = ScreenPercentage( hudOffset )
+        local hy = scrH - offset
+
+        -- Health BG
+        surface_SetDrawColor( bg )
+        surface_DrawRect( offset, hy, hw, hh )
+
+        plyHealth = Lerp( FrameTime() * 10, plyHealth, (ply:Health() / ply:GetMaxHealth()) * hw )
+        plyArmor = Lerp( FrameTime() * 10, plyArmor, (ply:Armor() / ply:GetMaxHealth()) * hw )
+
+        -- Health FG
+        surface_SetDrawColor( health )
+        surface_DrawRect( offset + 1, hy + 1, plyHealth - 1, hh - 2 )
+
+        local ah = ScreenPercentage( armorHeight )
+        local ay = hy - ah - healthArmorOffset
+
+        -- Armor BG
+        surface_SetDrawColor( bg )
+        surface_DrawRect( offset, ay, hw, ah )
+
+        -- Armor FG
+        surface_SetDrawColor( armor )
+        surface_DrawRect( offset + 1, ay + 1, plyArmor - 1, ah - 2 )
+    end)
+
+end)
+
+-- Disable default hud
+do
+
+    local hideElements = {
+        ['CHudHealth'] = true,
+        ['CHudBattery'] = true
+    }
+
+    hook.Add('HUDShouldDraw', addonName, function( name )
+        if hideElements[ name ] then
+            return false
+        end
+    end)
+
+end
